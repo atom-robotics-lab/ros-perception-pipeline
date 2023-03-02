2f1cbdecb838f1f18531f8d1ae98ea840494a151
Jasmeet Singh <jasmeet0915@gmail.com>
2023-03-03 02:21:58 +0530

    Cleaned up perception yolo script

--- scripts/perception_yolo_new.py
+++ scripts/perception_yolo_new.py
@@ -1,59 +1,40 @@
 #!/usr/bin/env python
 
-from inspect import FrameInfo
-import cv2
 import time
 import sys
-#from defer import return_value
+import os
+
+import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
+
 import rospy
 from sensor_msgs.msg import Image, CompressedImage
-import os
-import rospy
-
 from geometry_msgs.msg import PoseStamped
 from object_msgs.msg import ObjectPose
-from sensor_msgs.msg import Image
-# from find_object_2d.msg import ObjectsStamped, DetectionInfo
-
-from ebot_perception.srv import *
-
 from actionlib import SimpleActionServer
-#from ebot_handler.msg import PerceptionAction, PerceptionResult, PerceptionFeedbacks
-from rospy.exceptions import ROSException
-
-import roslaunch
-
-import message_filters
 
 
+# yolo constants add in ROS params
 INPUT_WIDTH = 640
 INPUT_HEIGHT = 640
 SCORE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
 CONFIDENCE_THRESHOLD = 0.25
 
+# to get path of model
 ROOT_DIR = os.getcwd()
 
-
+# bounding box colors
 colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
 
-is_cuda = len(sys.argv) > 1 and sys.argv[1] == "cuda"
-
-#net = build_model(is_cuda)
-#capture = load_capture()
-
-start = time.time_ns()
-frame_count = 0
-total_frames = 0
-fps = -1
-
+# is_cuda = len(sys.argv) > 1 and sys.argv[1] == "cuda"
 
 
 class WorkpieceDetector :
 
     def __init__(self):
+        # calculate fps, TODO: create a boolean to enable/diable show_fps
         self.frame_count = 0
         self.total_frames = 0
         self.fps = -1
@@ -67,8 +48,10 @@ class WorkpieceDetector :
         self.return_value = False
         self.objectid = None
         self.image_sub = rospy.Subscriber("/camera/color/image_raw2/compressed", CompressedImage, self.load_capture)
+        # maybe return final image with bounding box and fps instead of imshow
         self.yolo_service = rospy.Service('yolo_service', find_object, self.find_object_cb)
         
+    # yolo service callback that returns boolean True if object was detected, it calls the main control loop
     def find_object_cb(self, req) :
        self.object = req.object_name
        rospy.loginfo("Perception request received for {}".format(self.object))
@@ -77,7 +60,9 @@ class WorkpieceDetector :
 
        return find_objectResponse(self.return_value)
     
+    # load model and prepare its backend to either run on GPU or CPU, see if it can be added in constructor
     def build_model(self , is_cuda):
+        # TODO: use from ros param or launch param
         self.net = cv2.dnn.readNet("src/automate-robot-bvp/ebot_perception/scripts/utils/auto_final.onnx")
         if is_cuda:
             print("Attempty to use CUDA")
@@ -87,39 +72,28 @@ class WorkpieceDetector :
             print("Running on CPU")
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
             self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-        return self.net
 
-    def detect(self,image,net ):
-        self.net = net
+    def detect(self,image):
+        # convert image to 640x640 
         blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
         self.net.setInput(blob)
         preds = self.net.forward()
         return preds
 
     def load_capture(self, data):
-        
-        #self.capture = cv2.VideoCapture("sample.mp4")
-
         np_arr = np.fromstring(data.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         self.frame = image_np
         self.capture = self.frame        
 
-        # if self.flag == 0 and self.frame is not None:
-        #     #self.control_loop()
-        #     self.flag = 1
-        # else:
-        #     print("Control Loop ran once")
-
-
+    # load classes.txt that contains mapping of model with labels
     def load_classes(self):
         self.class_list = []
         with open("src/automate-robot-bvp/ebot_perception/scripts/utils/classes.txt", "r") as f:
             self.class_list = [cname.strip() for cname in f.readlines()]
         return self.class_list
 
-
-
+    # extract bounding box, class IDs and confidences of detected objects
     def wrap_detection(self,input_image, output_data):
         class_ids = []
         confidences = []
@@ -154,6 +128,7 @@ class WorkpieceDetector :
                     box = np.array([left, top, width, height])
                     boxes.append(box)
 
+        # removing intersecting bounding boxes
         indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45) 
 
         result_class_ids = []
@@ -167,12 +142,12 @@ class WorkpieceDetector :
 
         return result_class_ids, result_confidences, result_boxes
 
-    def format_yolov5(self,frame):
-
-        row, col, _ = frame.shape
+    # makes image square with dimension max(h, w)
+    def format_yolov5(self):
+        row, col, _ = self.frame.shape
         _max = max(col, row)
         result = np.zeros((_max, _max, 3), np.uint8)
-        result[0:row, 0:col] = frame
+        result[0:row, 0:col] = self.frame
         return result   
 
     def display_objects(self) :
@@ -188,61 +163,42 @@ class WorkpieceDetector :
 
     def control_loop(self) :
         
-        rospy.loginfo("Waiting 3 secs for frame")
-        #rospy.spin(3)
-
-        
-
         if self.frame is None :
             print("Frame is None")
-
+            # wait or possibly return?
         else:
+            # load model
+            self.build_model(is_cuda)
             
-        
-            self.net = self.build_model(is_cuda)
+            # load classes (object labels)
             self.load_classes()
+
+            # get index of object name
             self.objectid = self.class_list.index(self.object)
-            #self.capture = self.load_capture()
 
-                #while self.capture is not None:
-            #frame = self.capture
-            cv2.imwrite("frame.png", self.frame)
-            print("control_loop")
+            # make image square
+            inputImage = self.format_yolov5()
 
-            if self.frame is None:
-                print("End of stream")
-                exit()
-
-                
-            
-            
-            inputImage = self.format_yolov5(self.frame)
-            #resized = cv2.resize(inputImage , (640,640))
-            #blurred = cv2.blur(resized ,(10,10))
-            outs = self.detect(inputImage, self.net)
+            outs = self.detect(inputImage)
             class_ids, confidences, boxes = self.wrap_detection(inputImage, outs[0])
-            #print("ID : " , class_ids)
-            #print("Boxes : ",boxes)
+
             self.frame_count += 1
             self.total_frames += 1
-            self.objectid = self.class_list.index(self.object)
                             
-            print("Detected boxes : ", boxes)
             print("Detected ids : ", class_ids)
             
+            # if requested object is found
             if self.objectid in class_ids :
                 print("Item {} found".format(self.object))
                 index = class_ids.index(self.objectid)
                 box = boxes[index]
-                
-                
                 print(box)
-                
                 self.return_value = True
             else:
                 print("Item {} not found".format(self.object))
                 self.return_value = False
             
+            # draw bounding box and add label
             for (classid, confidence, box) in zip(class_ids, confidences, boxes):
                 color = colors[int(classid) % len(colors)]
                 cv2.rectangle(self.frame, box, color, 2)
@@ -252,9 +208,7 @@ class WorkpieceDetector :
                 except :
                     pass
             
-            #self.return_value=True
-            #else:
-            #rospy.loginfo("Object not found")
+            # fps
             if self.frame_count >= 30:
                 self.end = time.time_ns()
                 self.fps = 1000000000 * frame_count / (self.end - self.start)
@@ -263,48 +217,15 @@ class WorkpieceDetector :
             if self.fps > 0:
                 self.fps_label = "FPS: %.2f" % self.fps
                 cv2.putText(self.frame, fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)           
-        
-            #self.display_objects()
 
-            if self.frame is not None :
-                cv2.imshow("Object Detection", self.frame)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-            else:
-                rospy.loginfo("Bounding box frame is None")
-            
             return self.return_value
 
-
-# def yolo_perception_server() :
-#     rospy.init_node("perception_yolo")
-#     s = rospy.Service('yolo_service', find_object, find_object_cb)
-#     rospy.loginfo("yolo service active")
-#     rospy.spin()
-
-# def find_object_cb(req) :
-
-#     rospy.loginfo("Perception request received")
-#     wd = WorkpieceDetector(req.object_name)
-#     result = wd.control_loop()
-#     print("YOLO Result: {}".format(result))
-
-#     return find_objectResponse(wd.return_value)
-
-
-
-#print("Total frames: " + str(total_frames))
 
 if __name__ == "__main__" :
     rospy.init_node("perception_yolo")
 
     wd = WorkpieceDetector()
     rospy.spin()
-
-    # yolo_perception_server()
-
-    # yolo_perception_server()
-
     
     
 
