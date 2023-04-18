ec42df6efdf141d1f991a4ae799b5303735e3b9c
sanchayvashist <s.vast979@gmail.com>
2023-04-18 15:23:31 +0530

    Object detection models

---
+++ scripts/efficient_det.py
@@ -0,0 +1,141 @@
+import tensorflow_hub as hub
+import cv2
+import numpy
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+import tempfile
+
+
+# For drawing onto the image.
+import numpy as np
+from PIL import Image
+from PIL import ImageColor
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageOps
+
+# For measuring the inference time.
+import time 
+
+class mobile_net:
+    def __init__(self):
+        module_handle="https://tfhub.dev/tensorflow/efficientdet/d0/1"    
+        # Loading model directly from TensorFlow Hub
+        self.detector = hub.load(module_handle)
+        # Loading csv with labels of classes
+        self.labels = pd.read_csv('labels.csv', sep=';', index_col='ID')
+        self.labels = self.labels['OBJECT (2017 REL.)']
+        # Resizing image
+        self.img_height=800
+        self.img_width=800
+
+    def display_image(self,image):
+        cv2.imshow("result", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def draw_bounding_box_on_image(self,image,ymin,xmin,ymax,xmax,color,font,thickness=4,display_str_list=()):
+        """Adds a bounding box to an image."""
+        draw = ImageDraw.Draw(image)
+        im_width, im_height = image.size
+        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                        ymin * im_height, ymax * im_height)
+        draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
+                    (left, top)],
+                    width=thickness,
+                    fill=color)
+
+        # If the total height of the display strings added to the top of the bounding
+        # box exceeds the top of the image, stack the strings below the bounding box
+        # instead of above.
+        display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
+        # Each display_str has a top and bottom margin of 0.05x.
+        total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+        if top > total_display_str_height:
+            text_bottom = top
+        else:
+            text_bottom = top + total_display_str_height
+        # Reverse list and print from bottom to top.
+        for display_str in display_str_list[::-1]:
+            text_width, text_height = font.getsize(display_str)
+            margin = np.ceil(0.05 * text_height)
+            draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                            (left + text_width, text_bottom)],
+                        fill=color)
+            draw.text((left + margin, text_bottom - text_height - margin),
+                    display_str,
+                    fill="black",
+                    font=font)
+            text_bottom -= text_height - 2 * margin
+
+
+    def draw_boxes(self,image,boxes,class_names,scores,max_boxes=10,min_score=0.35):
+        """Overlay labeled boxes on an image with formatted scores and label names."""
+        colors = list(ImageColor.colormap.values())
+
+        try:
+            font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
+                                    25)
+        except IOError:
+            print("Font not found, using default font.")
+            font = ImageFont.load_default()
+
+        for i in range(min(boxes.shape[0], max_boxes)):
+            if scores[i] >= min_score:
+                ymin, xmin, ymax, xmax = tuple(boxes[i])
+                display_str = "{}: {}%".format(self.labels[class_names[i]],
+                                                int(100 * scores[i]))
+                color = colors[hash(class_names[i]) % len(colors)]
+                image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+                self.draw_bounding_box_on_image(
+                    image_pil,
+                    ymin,
+                    xmin,
+                    ymax,
+                    xmax,
+                    color,
+                    font,
+                    display_str_list=[display_str])
+                np.copyto(image, np.array(image_pil))
+        return image
+
+    def load_img(self,path):
+        img = tf.io.read_file(path)
+        img = tf.image.decode_jpeg(img, channels=3)
+        return img
+
+    def run_detector(self,detector, path):
+        img = cv2.imread(path)
+        inp = cv2.resize(img, (self.img_height,self.img_width))
+        #Convert img to RGB
+        rgb = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
+        # COnverting to uint8
+        rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+        #Add dims to rgb_tensor
+        rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+        start_time = time.time()
+        result = detector(rgb_tensor)
+        end_time = time.time()
+        result = {key:value.numpy() for key,value in result.items()}
+        print("Found %d objects." % len(result["detection_scores"]))
+        print("Inference time: ", end_time-start_time)
+        image_with_boxes = self.draw_boxes(
+            inp, result["detection_boxes"][0],
+            result["detection_classes"][0], result["detection_scores"][0])
+
+        self.display_image(image_with_boxes)
+
+
+    def detect_img(self,image_url):
+        start_time = time.time()
+        self.run_detector(self.detector, image_url)
+        end_time = time.time()
+        print("Inference time:",end_time-start_time)
+
+if __name__=='__main__':
+  # Load model 
+    det=mobile_net()
+    det.detect_img("/home/sanchay/yolo_catkin/src/yolov8_test/scripts/dog_cat.jpg")
