e6d9b77f812c6325079f7a44f08c883af694bea5
Jasmeet Singh <jasmeet0915@gmail.com>
2023-05-11 03:09:08 +0530

    Updated DetectorBase abstract class and Yolov5 Plugin

--- object_detection/object_detection/Detectors/YOLOv5.py
+++ object_detection/object_detection/Detectors/YOLOv5.py
@@ -7,48 +7,30 @@ from ..DetectorBase import DetectorBase
 
 
 class YOLOv5(DetectorBase):
-    def __init__(self, model_dir_path, weight_file_name, conf_threshold = 0.7, 
-                 score_threshold = 0.4, nms_threshold = 0.25, is_cuda = 0,
-                 show_fps = 1):
+    def __init__(self, conf_threshold = 0.7, score_threshold = 0.4, nms_threshold = 0.25, is_cuda = 0):
+
+        super().__init__()
 
         # opencv img input
         self.frame = None
         self.net = None
-        self.predictions = []
-        self.model_dir_path = model_dir_path
-        self.weight_file_name = weight_file_name
         self.INPUT_WIDTH = 640
         self.INPUT_HEIGHT = 640
         self.CONFIDENCE_THRESHOLD = conf_threshold
 
-        self.bb_colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
-        self.is_cuda = is_cuda      
-        self.show_fps = show_fps 
+        self.is_cuda = is_cuda   
 
-        if self.show_fps :
-            # calculate fps, TODO: create a boolean to enable/diable show_fps
-            self.frame_count = 0
-            self.total_frames = 0
-            self.fps = -1
-            self.start = time.time_ns()
-
-        
-        # load & build the given model
-        self.build_model(self.is_cuda)
-        
-        # load classes (object labels)
-        self.load_classes()
     
     # load model and prepare its backend to either run on GPU or CPU, see if it can be added in constructor
-    def build_model(self, is_cuda):
-        model_path = os.path.join(self.model_dir_path, self.weight_file_name)
+    def build_model(self, model_dir_path, weight_file_name):
+        model_path = os.path.join(model_dir_path, weight_file_name)
 
         try:
             self.net = cv2.dnn.readNet(model_path)
         except:
             raise Exception("Error loading given model from path: {}. Maybe the file doesn't exist?".format(model_path))
 
-        if is_cuda:
+        if self.is_cuda:
             print("is_cuda was set to True. Attempting to use CUDA")
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
             self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
@@ -57,6 +39,16 @@ class YOLOv5(DetectorBase):
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
             self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
+
+    # load classes.txt that contains mapping of model with labels
+    # TODO: add try/except to raise exception that tells the use to check the name if it is classes.txt
+    def load_classes(self, model_dir_path):
+        self.class_list = []
+        with open(model_dir_path + "/classes.txt", "r") as f:
+            self.class_list = [cname.strip() for cname in f.readlines()]
+        return self.class_list
+
+    
     def detect(self, image):
         # convert image to 640x640 
         blob = cv2.dnn.blobFromImage(image, 1/255.0, (self.INPUT_WIDTH, self.INPUT_HEIGHT), swapRB=True, crop=False)
@@ -64,13 +56,6 @@ class YOLOv5(DetectorBase):
         preds = self.net.forward()
         return preds
 
-    # load classes.txt that contains mapping of model with labels
-    # TODO: add try/except to raise exception that tells the use to check the name if it is classes.txt
-    def load_classes(self):
-        self.class_list = []
-        with open(self.model_dir_path + "/classes.txt", "r") as f:
-            self.class_list = [cname.strip() for cname in f.readlines()]
-        return self.class_list
 
     # extract bounding box, class IDs and confidences of detected objects
     # YOLOv5 returns a 3D tensor of dimension 25200*(5 + n_classes)
@@ -133,6 +118,7 @@ class YOLOv5(DetectorBase):
 
         return result_class_ids, result_confidences, result_boxes
 
+
     # makes image square with dimension max(h, w)
     def format_yolov5(self):
         row, col, _ = self.frame.shape
@@ -141,15 +127,6 @@ class YOLOv5(DetectorBase):
         result[0:row, 0:col] = self.frame
         return result
 
-    def create_predictions_list(self, class_ids, confidences, boxes):
-        for i in range(len(class_ids)):
-            obj_dict = {
-                "class_id": class_ids[i],
-                "confidence": confidences[i],
-                "box": boxes[i]
-            }
-
-            self.predictions.append(obj_dict)
 
     def get_predictions(self, cv_image):
         if cv_image is None:
@@ -164,38 +141,12 @@ class YOLOv5(DetectorBase):
             outs = self.detect(inputImage)
             class_ids, confidences, boxes = self.wrap_detection(inputImage, outs[0])
 
-            self.create_predictions_list(class_ids, confidences, boxes)
-
-            self.frame_count += 1
-            self.total_frames += 1
+            super().create_predictions_list(class_ids, confidences, boxes)
                             
-            print("Detected ids: ", class_ids)
+            print("Detected ids: ", class_ids)         
             
-            # draw bounding box and add label
-            for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-                color = self.bb_colors[int(classid) % len(self.bb_colors)]
-                cv2.rectangle(self.frame, box, color, 2)
-                cv2.rectangle(self.frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-                try :
-                    cv2.putText(self.frame, self.class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
-                except :
-                    pass
-            
-            # fps
-            if self.show_fps :
-
-                if self.frame_count >= 30:
-                    self.end = time.time_ns()
-                    self.fps = 1000000000 * self.frame_count / (self.end - self.start)
-                    self.frame_count = 0
-                    self.start = time.time_ns()
-
-                if self.fps > 0:
-                    self.fps_label = "FPS: %.2f" % self.fps
-                    cv2.putText(self.frame, self.fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)           
-            
-            return (self.predictions, self.frame)
+            return self.predictions
     
 
-def register(model_dir_path, weight_file_name):
-    return YOLOv5(model_dir_path, weight_file_name)
+def register():
+    return YOLOv5()
