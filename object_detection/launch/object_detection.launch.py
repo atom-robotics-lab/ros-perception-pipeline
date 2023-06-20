2c8152d6a5ae5b5f6e3541648ae97d9ba79ac6a9
Arjun K Haridas <51917087+topguns837@users.noreply.github.com>
2023-06-20 21:11:15 +0530

    added launch file and params.yaml (#14)
    * added launch file and params.yaml
    
    * migrated from garden --> fortress

---
+++ object_detection/launch/object_detection.launch.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_object_detection = get_package_share_directory("object_detection")
+
+    params = os.path.join(
+        pkg_object_detection,
+        'config',
+        'params.yaml'
+        )
+    
+    node=Node(
+        package = 'object_detection',
+        name = 'object_detection',
+        executable = 'ObjectDetection',
+        parameters = [params]
+    )
+  
+
+    return LaunchDescription([node])
