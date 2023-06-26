321df9f4c7667829a40e0f76b7cf80f53e50506a
sanchayvashist <s.vast979@gmail.com>
2023-06-26 08:17:44 +0530

    plugin architecture for yolov8

--- object_detection/object_detection/Detectors/EfficientDet.py
+++ object_detection/object_detection/Detectors/EfficientDet.py
@@ -1,196 +1,102 @@
-import tensorflow_hub as hub
+
 import cv2
-import numpy
-import pandas as pd
 import tensorflow as tf
-import matplotlib.pyplot as plt
-
-import tempfile
-
-
-# For drawing onto the image.
+import os
 import numpy as np
-from PIL import Image
-from PIL import ImageColor
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
 
 # For measuring the inference time.
 import time 
 
 class EfficientDet:
-    def __init__(self, model_dir_path, weight_file_name, conf_threshold = 0.7, score_threshold = 0.25, nms_threshold = 0.4):
-        
-        self.frame_count = 0
-        self.total_frames = 0
-        self.fps = -1
-        self.start = time.time_ns()
-        self.frame = None
+    def __init__(self, model_dir_path, weight_file_name, conf_threshold = 0.35, score_threshold = 0.25, nms_threshold = 0.4):
         
         self.model_dir_path = model_dir_path
-        self.weight_file_name = weight_file_name
-        self.conf=conf_threshold
+        self.weight_file_name = weight_file_name        
+
+        self.conf = conf_threshold
         
-        # Resizing image
-        self.img_height=800
-        self.img_width=800
-        self.predictions=[]
+        self.img_height = 640
+        self.img_width = 640
+        self.predictions = []
+        self.bb_colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
 
         self.build_model()
         self.load_classes()
 
     def build_model(self) :
-        module_handle="https://tfhub.dev/tensorflow/efficientdet/d0/1"
-        # Loading model directly from TensorFlow Hub
-        self.detector = hub.load(module_handle)
-
-
+        model_path = os.path.join(self.model_dir_path, self.weight_file_name)
+        try :
+            self.detector = tf.saved_model.load(model_path)
+        except :
+            raise Exception("Error loading given model from path: {}. Maybe the file doesn't exist?".format(model_path))
+        
     def load_classes(self):
         self.labels = []
         with open(self.model_dir_path + "/classes.txt", "r") as f:
             self.labels = [cname.strip() for cname in f.readlines()]
         return self.labels
 
-    def display_image(self,image):
-        cv2.imshow("result", image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    def draw_bounding_box_on_image(self,image,ymin,xmin,ymax,xmax,color,font,thickness=4,display_str_list=()):
-        """Adds a bounding box to an image."""
-        draw = ImageDraw.Draw(image)
-        im_width, im_height = image.size
-        (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
-                                        ymin * im_height, ymax * im_height)
-        draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
-                    (left, top)],
-                    width=thickness,
-                    fill=color)
-        # If the total height of the display strings added to the top of the bounding
-        # box exceeds the top of the image, stack the strings below the bounding box
-        # instead of above.
-        display_str_heights = [font.getsize(ds)[1] for ds in display_str_list]
-        # Each display_str has a top and bottom margin of 0.05x.
-        total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
-
-        if top > total_display_str_height:
-            text_bottom = top
-        else:
-            text_bottom = top + total_display_str_height
-        # Reverse list and print from bottom to top.
-        for display_str in display_str_list[::-1]:
-            text_width, text_height = font.getsize(display_str)
-            margin = np.ceil(0.05 * text_height)
-            draw.rectangle([(left, text_bottom - text_height - 2 * margin),
-                            (left + text_width, text_bottom)],
-                        fill=color)
-            draw.text((left + margin, text_bottom - text_height - margin),
-                    display_str,
-                    fill="black",
-                    font=font)
-            text_bottom -= text_height - 2 * margin
-
+   
     # create list of dictionary containing predictions
     def create_predictions_list(self, class_ids, confidences, boxes):
         for i in range(len(class_ids)):
-            obj_dict = {
-                "class_id": class_ids[i],
-                "confidence": confidences[i],
-                "box": boxes[i]
-            }
-            self.predictions.append(obj_dict)
-
-    def draw_boxes(self,image,boxes,class_ids,confidences,max_boxes=10):
-        """Overlay labeled boxes on an image with formatted scores and label names."""
-        colors = list(ImageColor.colormap.values())
-
-        try:
-            font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSansNarrow-Regular.ttf",
-                                    25)
-        except IOError:
-            print("Font not found, using default font.")
-            font = ImageFont.load_default()
-
-        for i in range(min(boxes.shape[0], max_boxes)):
             if confidences[i] >= self.conf:
-                ymin, xmin, ymax, xmax = tuple(boxes[i])
-                display_str = "{}: {}%".format(self.labels[class_ids[i]], int(100 * confidences[i]))
-                color = colors[hash(class_ids[i]) % len(colors)]
-                image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
-                self.draw_bounding_box_on_image(image_pil,ymin,xmin,ymax,xmax,color,font,display_str_list=[display_str])
-                np.copyto(image, np.array(image_pil))
-        return image
+                obj_dict = {
+                    "class_id": class_ids[i],
+                    "confidence": confidences[i],
+                    "box": boxes[i]
+                }
+                self.predictions.append(obj_dict)
 
-    def load_img(self,path):
-        img = tf.io.read_file(path)
-        img = tf.image.decode_jpeg(img, channels=3)
-        return img
+    def rescale_box(self,pred_boxes):
+        rescaled_boxes = []
+
+        # rescale boxes
+        for box in pred_boxes:
+            ymin, xmin, ymax, xmax = tuple(box)
+            (left, right, top, bottom) = (xmin * self.image_width, xmax * self.image_width,
+                                            ymin * self.image_height, ymax * self.image_height)
+            rescaled_box = np.array([left,top,right,bottom]).astype(int)
+            rescaled_boxes.append(rescaled_box)    
+
+        return rescaled_boxes
 
     def get_predictions(self,cv_image):
 
-        if cv_image is None:
-            # TODO: show warning message (different color, maybe)
-            return None,None
-        
-        else :
-            #Convert img to RGB
-            self.frame = cv_image
+        #Convert img to RGB
+        frame = cv_image.copy()
 
-            self.frame_count += 1
-            self.total_frames += 1
+        # Image dimensions
+        self.image_height, self.image_width, _ = frame.shape
 
-            rgb = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # COnverting to uint8
-            rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
-
-            #Add dims to rgb_tensor
-            rgb_tensor = tf.expand_dims(rgb_tensor , 0)
-
-            
-            result = self.detector(rgb_tensor)
-            result = {key:value.numpy() for key,value in result.items()}
-
-            self.create_predictions_list(result["detection_boxes"][0],result["detection_classes"][0], result["detection_scores"][0])
-            image_with_boxes = self.draw_boxes(cv_image,result["detection_boxes"][0],result["detection_classes"][0], result["detection_scores"][0])
-
-            # fps
-            if self.frame_count >= 30:
-                self.end = time.time_ns()
-                self.fps = 1000000000 * self.frame_count / (self.end - self.start)
-                self.frame_count = 0
-                self.start = time.time_ns()
-
-            if self.fps > 0:
-                self.fps_label = "FPS: %.2f" % self.fps
-                cv2.putText(self.frame, self.fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)           
-
-            return [self.predictions, image_with_boxes]
-
-
-    def detect_img(self,image_url):
-        start_time = time.time()
-        self.run_detector(self.detector, image_url)#Convert img to RGB
-        rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
         # COnverting to uint8
         rgb_tensor = tf.convert_to_tensor(rgb, dtype=tf.uint8)
+
         #Add dims to rgb_tensor
         rgb_tensor = tf.expand_dims(rgb_tensor , 0)
+
         start_time = time.time()
         result = self.detector(rgb_tensor)
         end_time = time.time()
+
+        # refactor resutls
         result = {key:value.numpy() for key,value in result.items()}
-        print("Found %d objects." % len(result["detection_scores"]))
+        result["detection_boxes"][0] = self.rescale_box(result["detection_boxes"][0])
+        result["detection_classes"][0] = result["detection_classes"][0] -1
+
+        print("Found %d objects." % len(result["detection_scores"][0]))
         print("Inference time: ", end_time-start_time)
-        self.create_predictions_list(cv_image,result["detection_boxes"][0],result["detection_classes"][0], result["detection_scores"][0])
-        image_with_boxes = self.draw_boxes(cv_image,result["detection_boxes"][0],result["detection_classes"][0], result["detection_scores"][0])
-        self.display_image(self.predictions,image_with_boxes)
+        
+        self.create_predictions_list(result["detection_classes"][0], result["detection_scores"][0], result["detection_boxes"][0].astype(int))
 
-        end_time = time.time()
-        print("Inference time:",end_time-start_time)
+        # draw bounding box and add label
+        
+        # for (classid, confidence, box) in zip(result["detection_classes"][0], result["detection_scores"][0], result["detection_boxes"][0].astype(int)):
+        #     if confidence >= self.conf:
+        #         color = self.bb_colors[int(classid) % len(self.bb_colors)]
+        #         cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color, 2)
+        #         cv2.putText(frame, f"{self.labels[int(classid)]} {confidence:.2f}", (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0),2)
 
-if __name__=='__main__':
-  # Load model 
-    det = EfficientDet()
-    det.detect_img("/home/sanchay/yolo_catkin/src/yolov8_test/scripts/dog_cat.jpg")
+        return self.predictions
--- object_detection/object_detection/Detectors/YOLOv8.py
+++ object_detection/object_detection/Detectors/YOLOv8.py
@@ -3,15 +3,13 @@ from ultralytics import YOLO
 import os
 import time
 
-class YOLOv8:
-  def __init__(self, model_dir_path, weight_file_name, conf_threshold = 0.7, 
-               score_threshold = 0.4, nms_threshold = 0.25,
-               show_fps = 1, is_cuda = 0):
-    
-    self.model_dir_path = model_dir_path
-    self.weight_file_name = weight_file_name
+from ..DetectorBase import DetectorBase
 
-    
+
+
+class YOLOv8(DetectorBase):
+  def __init__(self, conf_threshold = 0.7, score_threshold = 0.4, nms_threshold = 0.25, show_fps = 1, is_cuda = 0):
+  
     self.conf_threshold = conf_threshold
     self.show_fps = show_fps
     self.is_cuda = is_cuda
@@ -30,20 +28,20 @@ class YOLOv8:
     self.load_classes()
 
 
-  def build_model(self) :
+  def build_model(self, model_dir_path, weight_file_name) :
 
     try :
-      model_path = os.path.join(self.model_dir_path, self.weight_file_name)
+      model_path = os.path.join(model_dir_path, weight_file_name)
       self.model = YOLO(model_path)
     
     except :
       raise Exception("Error loading given model from path: {}. Maybe the file doesn't exist?".format(model_path))
   
-  def load_classes(self):
+  def load_classes(self, model_dir_path):
 
     self.class_list = []
 
-    with open(self.model_dir_path + "/classes.txt", "r") as f:
+    with open(model_dir_path + "/classes.txt", "r") as f:
         self.class_list = [cname.strip() for cname in f.readlines()]
 
     return self.class_list
@@ -82,22 +80,10 @@ class YOLOv8:
         bb.append(box.xyxy)
 
       self.create_predictions_list(class_id,confidence,bb)
-      result = self.model.predict(self.frame, conf = self.conf_threshold)
-      output_frame = result[0].plot()                  # Frame with bounding boxes
+      
+    #  output_frame = result[0].plot()                  Frame with bounding boxes
 
       print("frame_count : ", self.frame_count)
 
-
-      if self.show_fps :
-        if self.frame_count >= 30:
-          self.end = time.time_ns()
-          self.fps = 1000000000 * self.frame_count / (self.end - self.start)
-          self.frame_count = 0
-          self.start = time.time_ns()
-
-        if self.fps > 0:
-          self.fps_label = "FPS: %.2f" % self.fps
-          cv2.putText(output_frame, self.fps_label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-      return self.predictions, output_frame
+      return self.predictions
   
