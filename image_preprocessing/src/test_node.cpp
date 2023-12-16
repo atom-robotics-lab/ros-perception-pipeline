ae51a4e50b5c862b2262cf7bb4ab045742d98c53
Krachitkumar <sairachit123@gmail.com>
2023-12-16 13:21:35 +0530

    resolved cv_bridge error

--- image_preprocessing/src/test_node.cpp
+++ image_preprocessing/src/test_node.cpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/image.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include "sensor_msgs/image_encodings.hpp"
+#include <opencv2/opencv.hpp>
 
 class ImagePublisherNode : public rclcpp::Node {
 public:
@@ -9,7 +12,6 @@ public:
             "/color_camera/image_raw", 10, [this](const sensor_msgs::msg::Image::SharedPtr msg) {
                 // Callback function for the subscription
                 publishImage(msg);
-               // std::cout<<"Publishing Image"<<endl;
             });
 
         // Create a publisher for the "img_pub" topic
@@ -24,9 +26,21 @@ public:
 
 private:
     void publishImage(const sensor_msgs::msg::Image::SharedPtr msg) {
-        auto loaned_msg = std::make_unique<sensor_msgs::msg::Image>(*msg);
-        imagepublisher->publish(std::move(loaned_msg));
-        
+        // Convert sensor_msgs::Image to cv::Mat using cv_bridge
+        cv_bridge::CvImageConstPtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        } catch (cv_bridge::Exception& e) {
+            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        // Add text to the image
+        cv::putText(cv_ptr->image, "Img Processed", cv::Point(10, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 255, 255), 2);
+
+        // Publish the modified image
+        auto modified_msg = cv_bridge::CvImage(msg->header, "bgr8", cv_ptr->image).toImageMsg();
+        imagepublisher->publish(modified_msg);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr imagesubscription;
