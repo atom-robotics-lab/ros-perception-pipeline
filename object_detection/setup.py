640747fee2fa5ad1f850547749fb44b53a9f79ff
topguns837 <arjun.k@orangewood.co>
2024-02-11 15:31:23 +0530

    Add FPS calculations and added License and description to setup.cfg

--- object_detection/setup.py
+++ object_detection/setup.py
@@ -21,8 +21,8 @@ setup(
     zip_safe=True,
     maintainer='singh',
     maintainer_email='jasmeet0915@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Plug-and-Play ROS 2 package for Perception in Robotics',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
